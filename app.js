9f67f44c82639df9ea5479b62a83065fe5392e7b
Omar <164964816+8omz@users.noreply.github.com>
2024-07-09 12:09:34 +0000

    Updated RPS game.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let humanScore = 0
 let ComputerScore = 0
+let rounds = 0
 // Define inputs
 const choices = ['rock', 'paper', 'scissors']
 
 // Get the computers choice
 function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * 3)
+    let compChoice = Math.floor(Math.random() * 1)
     console.log(compChoice)
     return compChoice
 }
@@ -45,27 +46,26 @@ function playGame() {
 
         }
     }
-    playRound(getHumanChoice(), getComputerChoice())
-    console.log("this is the humans score:" + humanScore)
-    console.log("computers score:" + ComputerScore)
-    playRound(getHumanChoice(), getComputerChoice())
-    console.log("this is the humans score:" + humanScore)
-    console.log("computers score:" + ComputerScore)
-    playRound(getHumanChoice(), getComputerChoice())
-    console.log("this is the humans score:" + humanScore)
-    console.log("computers score:" + ComputerScore)
-    playRound(getHumanChoice(), getComputerChoice())
-    console.log("this is the humans score:" + humanScore)
-    console.log("computers score:" + ComputerScore)
-    playRound(getHumanChoice(), getComputerChoice())
-    console.log("this is the humans score:" + humanScore)
-    console.log("computers score:" + ComputerScore)
+    while(rounds <= 4 && humanScore !== 3 && ComputerScore !== 3){
+        console.log("this is your score"+ humanScore  +"\n"+ "This is the computers score"+ComputerScore)
+        playRound(getHumanChoice(), getComputerChoice())
+        rounds++
+        console.log(rounds)
+    }
 
     if (humanScore === 3 ){
         alert("YOU WIN!")
     }else if (ComputerScore === 3){
         alert("You lost")
-    }else if()
+    }else if (rounds => 4 && ComputerScore !==3 && humanScore !==3){
+        alert("Nobody Won in time, this is a draw.")
+    }
 }
 
-playGame()
+const startbtn = document.querySelector(".start")
+startbtn.addEventListener("click", () =>{
+    rounds = 0
+    humanScore = 0
+    ComputerScore = 0
+})
+startbtn.addEventListener("click", playGame)
