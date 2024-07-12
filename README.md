61a4a6f858e1a09b589b5a2530c8e90311e2b72f
Omar <164964816+8omz@users.noreply.github.com>
2024-07-12 11:41:19 +0000

    fully implemented UI

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RockpaperScirs
+
+
--- app.js
+++ app.js
@@ -1,71 +1,99 @@
 let humanScore = 0
 let ComputerScore = 0
 let rounds = 0
-// Define inputs
-const choices = ['rock', 'paper', 'scissors']
+let value;
+const para = document.querySelector(".para")
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+const hScore = document.querySelector(".humanScore")
+const cScore = document.querySelector(".compScore")
+const winLoseBox = document.querySelector(".winLoseContainer")
+const winLoseText = document.querySelector('.winLoseText')
+const finalScreen = document.querySelector('.finalScreen')
+const btnRestart = document.querySelector('.restart')
+const userPic = document.querySelector('.pic1')
+const compPic = document.querySelector('.pic2')
+
+function winLoseDraw(result) {
+    para.textContent = result
+}
 
 // Get the computers choice
 function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * 1)
-    console.log(compChoice)
-    return compChoice
-}
-// Get player choice
-function getHumanChoice() {
-    let value = prompt(`Chose between rock paper and scissors;
-        Rock = 1, Paper = 2, Scissors = 3`)
-    return value - 1
-}
+    let compChoice = Math.floor(Math.random() * 3)
+    if (compChoice == 0) {
+        compPic.src = "public/rock.png"
+    } else if (compChoice == 1){
+        compPic.src = "public/paper.png"
 
-function playGame() {
-    function playRound(hum, comp) {
-        if (hum === comp) {
-            console.log("equal values")
-
-        } else if (hum === 0 && comp === 2) {
-            humanScore += 1
-            console.log("Rock beats scissors u win")
-        } else if (hum === 1 && comp === 0) {
-            humanScore += 1
-            console.log("Paper beats Rock u win")
-
-        } else if (hum === 2 && comp === 1) {
-            humanScore += 1
-            console.log("Scissors beats paper u win")
-
-        } else if (comp === 0 && hum === 2) {
-            ComputerScore += 1
-            console.log("Rock beats scissors u lose")
-        } else if (comp === 1 && hum === 0) {
-            ComputerScore += 1
-            console.log("Paper beats Rock u lose")
-
-        } else if (comp === 2 && hum === 1) {
-            ComputerScore += 1
-            console.log("Scissors beats paper u lose")
-
-        }
+    }else if(compChoice==2){
+        compPic.src = "public/scissors.png"
     }
-    while(rounds <= 4 && humanScore !== 3 && ComputerScore !== 3){
-        console.log("this is your score"+ humanScore  +"\n"+ "This is the computers score"+ComputerScore)
-        playRound(getHumanChoice(), getComputerChoice())
-        rounds++
-        console.log(rounds)
-    }
+        return compChoice
 
-    if (humanScore === 3 ){
-        alert("YOU WIN!")
-    }else if (ComputerScore === 3){
-        alert("You lost")
-    }else if (rounds => 4 && ComputerScore !==3 && humanScore !==3){
-        alert("Nobody Won in time, this is a draw.")
+}
+// Plays a singular round, checks player input in comparison to the computers random input, after 5 wins a winner is chosen
+function playRound(hum, comp) {
+    if (hum === comp) {
+        winLoseDraw("Draw! You made the same selection")
+
+    } else if (hum === 0 && comp === 2) {
+        humanScore += 1
+        winLoseDraw("Rock beats scissors u win")
+    } else if (hum === 1 && comp === 0) {
+        humanScore += 1
+        winLoseDraw("Paper beats Rock u win")
+
+    } else if (hum === 2 && comp === 1) {
+        humanScore += 1
+        winLoseDraw("Scissors beats paper u win")
+
+    } else if (comp === 0 && hum === 2) {
+        ComputerScore += 1
+        winLoseDraw("Rock beats scissors u lose")
+    } else if (comp === 1 && hum === 0) {
+        ComputerScore += 1
+        winLoseDraw("Paper beats Rock u lose")
+
+    } else if (comp === 2 && hum === 1) {
+        ComputerScore += 1
+        winLoseDraw("Scissors beats paper u lose")
+
+    }
+    hScore.textContent = `Your score: ${humanScore}`
+    cScore.textContent = `Computer score: ${ComputerScore}`
+    if (humanScore == 5) {
+        winLoseText.textContent = "YOU WIN!"
+        winLoseBox.style.background = "rgba(72, 255, 0, 0.836)"
+        finalScreen.classList.remove("hidden")
+        finalScreen.classList.add("show")
+    } else if (ComputerScore == 5) {
+        winLoseText.textContent = "YOU LOSE!"
+        winLoseBox.style.background = "rgba(255, 0, 0, 0.836)"
+        finalScreen.classList.remove("hidden")
+        finalScreen.classList.add("show")
     }
 }
-
-const startbtn = document.querySelector(".start")
-startbtn.addEventListener("click", () =>{
-    rounds = 0
-    humanScore = 0
-    ComputerScore = 0
+rock.addEventListener("click", () => {
+    value = 0
+    userPic.src = "public/rock.png"
+    playRound(value, getComputerChoice())
 })
-startbtn.addEventListener("click", playGame)
+paper.addEventListener("click", () => {
+    value = 1
+    userPic.src = "public/paper.png"
+
+    playRound(value, getComputerChoice())
+})
+scissors.addEventListener("click", () => {
+    value = 2
+    userPic.src = "public/scissors.png"
+
+    playRound(value, getComputerChoice())
+})
+
+// restarts the game 
+btnRestart.addEventListener("click", () => {
+    window.location.reload();
+})
